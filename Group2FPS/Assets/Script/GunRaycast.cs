fb2046aec8b9b24a8a552735b18bedae05ab5307
Ethan5899 <35109417+Ethan5899@users.noreply.github.com>
2018-04-11 08:26:57 -0700

    EP

---
+++ Group2FPS/Assets/Script/GunRaycast.cs
@@ -0,0 +1,53 @@
+﻿
+using UnityEngine;
+
+public class GunRaycast : MonoBehaviour {
+    public float damage = 10;
+    public float range = 100;
+    public float shotCooldown = .1f;
+    public Camera FPSCam;
+    public ParticleSystem muzzelFlash;
+    private float timer;
+    private bool onCooldown = false, isShooting = false;
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && onCooldown == false)
+        {
+            isShooting = true;
+            muzzelFlash.Play();
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            isShooting = false;
+            muzzelFlash.Stop();
+        }
+        if (onCooldown == true)
+        {
+            timer += Time.deltaTime;
+            if(timer >= shotCooldown)
+            {
+                onCooldown = false;
+                timer = 0;
+            }
+        }
+        if(isShooting == true)
+        {
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+
+        RaycastHit hitInfo;
+        if (Physics.Raycast(FPSCam.transform.position, FPSCam.transform.forward, out hitInfo, range))
+        {
+            Debug.Log(hitInfo.transform.name);
+            EnemyTakeDamage target = hitInfo.transform.GetComponent<EnemyTakeDamage>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+                onCooldown = true;
+            }
+        }
+    }
+}
