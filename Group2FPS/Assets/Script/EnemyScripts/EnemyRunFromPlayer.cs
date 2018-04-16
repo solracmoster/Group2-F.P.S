502f124f73316272ebcc5995d8c94f623c0dba0e
Ethan5899 <35109417+Ethan5899@users.noreply.github.com>
2018-04-16 08:16:31 -0700

    EP

--- Group2FPS/Assets/Script/EnemyScripts/EnemyRunFromPlayer.cs
+++ Group2FPS/Assets/Script/EnemyScripts/EnemyRunFromPlayer.cs
@@ -2,23 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-public class EnemyChasePlayer : MonoBehaviour {
+public class EnemyRunFromPlayer : MonoBehaviour {
     NavMeshAgent agent;
     public GameObject player;
-    public float triggerDistance = 100;
     private float distance;
-	// Use this for initialization
-	void Start () {
+    public float triggerDistance = 100;
+    // Use this for initialization
+    void Start()
+    {
         agent = gameObject.GetComponent<NavMeshAgent>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         distance = (gameObject.transform.position - player.transform.position).magnitude;
-        if (distance <= triggerDistance)
+        if(distance <= triggerDistance)
         {
-            agent.destination = player.transform.position;
+            agent.destination = gameObject.transform.position - player.transform.position;
         }
-            
-	}
+    }
 }
