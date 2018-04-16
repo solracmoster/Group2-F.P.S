709fb26203ec07c2bc6fba580a42ef3678fe7c81
Carlos Magana <18.carlos.magana@ksd.org>
2018-04-16 09:09:50 -0700

    Added Punchman

---
+++ Group2FPS/Assets/Bunch of Carlos's/Melee.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Melee : MonoBehaviour
+{
+
+    GameObject prefab;
+    void Start()
+    {
+        prefab = Resources.Load("projectile") as GameObject;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+        {
+            GameObject projectile = Instantiate(prefab) as GameObject;
+            projectile.transform.position = transform.position + Camera.main.transform.forward * 2;
+            Rigidbody rb = projectile.GetComponent<Rigidbody>();
+            rb.velocity = Camera.main.transform.forward * 20;
+        }
+    }
+}
